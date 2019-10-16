0d24a9320803f5f1c1025a33d249be5294dc172a
Ariel Acha <arielburlatacha@gmail.com>
2019-10-17 07:19:55 +0800

    added admin model

---
+++ application/models/Coin_solve_model_admin.php
@@ -0,0 +1,49 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Coin_solve_model_admin extends CI_Model { 
+
+	public function get_all_user_count(){
+
+		$query = $this
+                ->db
+                ->get('users');
+    
+        return $query->num_rows(); 
+
+	}
+
+	public function get_all_withdrawal_count() {
+
+		$query = $this
+                ->db
+                ->get_where('withdrawals' , array('withdrawal_status' => 'Pending'));
+    
+        return $query->num_rows(); 
+
+	}
+
+	public function get_all_games_played_count_today() {
+
+		$query = $this
+                ->db
+                ->get('game_details');
+    
+        return $query->num_rows(); 
+
+	}
+
+	public function get_total_users_earned () {
+
+		$this->db->select_sum('score');
+
+		$query = $this
+                ->db
+                ->get('game_details');
+    
+        return $query->row()->score; 
+
+	}
+
+}
