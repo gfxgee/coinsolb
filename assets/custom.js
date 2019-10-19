9af704dcb2e982a9d32d01897c00db55644fcf36
Ariel Acha <arielburlatacha@gmail.com>
2019-10-19 09:50:46 +0800

    data table migration

--- assets/custom.js
+++ assets/custom.js
@@ -13,48 +13,6 @@
   }
 
 $(document).ready(function() {
-    $('#score-table').DataTable({
-    	"pageLength" : 25,
-        "order" : [],
-        "ajax": {
-            url : "coin_solve/get_user_points_history",
-            type : 'GET'
-        },
-        columns: [
-            { title: "Date" },
-            { title: "Score" },
-            { title: "Earned From" }
-        ]
-    });
-
-    $('#referrals-table').DataTable({
-        "pageLength" : 25,
-        "order" : [],
-        "ajax": {
-            url : "coin_solve/get_user_referrals_history",
-            type : 'GET'
-        },
-        columns: [
-            { title: "Name" },
-            { title: "Email" },
-            { title: "Status" }
-        ]
-    });
-
-    $('#withdrawals-table').DataTable({
-        "pageLength" : 25,
-        "order" : [],
-        "ajax": {
-            url : "coin_solve/get_user_withdrawals_history",
-            type : 'GET'
-        },
-        columns: [
-            { title: "Date" },
-            { title: "Type of Withdrawal"},
-            { title: "Amount" },
-            { title: "Status" }
-        ]
-    });
 
     $('#select-payment').change(function(){
 
