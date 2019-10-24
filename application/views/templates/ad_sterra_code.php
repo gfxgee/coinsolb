faf4766d63709a32302cf369586a52e73699c2f8
Ariel Acha <arielburlatacha@gmail.com>
2019-10-24 21:51:27 +0800

    try ad sterra

---
+++ application/views/templates/ad_sterra_code.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '9c94dd07cb19027c546992e6cd0f8007',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bcloudhost.com/9c94dd07cb19027c546992e6cd0f8007/invoke.js"></scr' + 'ipt>');
+</script>
