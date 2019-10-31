307f68fa947d1ade95372b1c81565c7aeacf1aa5
Ariel Acha <arielburlatacha@gmail.com>
2019-11-01 06:36:00 +0800

    add ad

---
+++ application/views/templates/main_ad.php
@@ -0,0 +1,11 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Main Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9239016295634478"
+     data-ad-slot="5313924712"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
