31742f56a5a2c0a560c2c5826147667e5cb9f31e
Ariel Acha <arielburlatacha@gmail.com>
2019-11-01 13:13:33 +0800

    ads

--- application/views/play.php
+++ application/views/play.php
@@ -46,7 +46,7 @@
 	
 		<div class="row bg-secondary-color text-white" id="game-container">
 			<div class="col-sm-12 mb-4 m-auto p-5">
-				<?php $this->load->view('templates/ad_header'); ?>
+				<?php $this->load->view('templates/ad_sterra_code'); ?>
 			</div>
 			<div class="col-sm-2 p-3">
 				<?php  $this->load->view('templates/ad_sterra_code'); ?>
