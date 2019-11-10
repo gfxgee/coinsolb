c9fbdd60e7ebee6dd0248feb382481bce12d5b44
Ariel Acha <arielburlatacha@gmail.com>
2019-11-11 05:17:29 +0800

    Update Coin_solve.php

--- application/controllers/Coin_solve.php
+++ application/controllers/Coin_solve.php
@@ -155,9 +155,7 @@ class Coin_solve extends CI_Controller  {
 	}
 
 	public function play () 
-	{	
-
-                redirect('dashboard','refresh');
+	{
 
 		$meta_description = 'Play to earn points while enhancing you math skills with Coinsolb and get some awesomes rewards from the points you earned.';
 
